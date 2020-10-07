d6d2603c45b55b7ab7ae7555025d6c387a751cb4
nhocbaby76 <chidung091@gmail.com>
2020-10-07 13:53:48 +0700

    Remove show password field when login

--- app/src/main/java/com/chidung091/chamcongvn/MainActivity.java
+++ app/src/main/java/com/chidung091/chamcongvn/MainActivity.java
@@ -1,13 +1,13 @@
 package com.chidung091.chamcongvn;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     Button bt1;
